33cdc125ceb1a114cf76bf4a8099de157b364101
George <georgekutsia@gmail.com>
2023-09-27 11:26:57 +0100

    corregido overboard

--- src/components/navbar/NavbarCenterComponent.jsx
+++ src/components/navbar/NavbarCenterComponent.jsx
@@ -9,10 +9,10 @@ function NavbarCenterComponent() {
 
   return (
     <div className='navbarBox'>
-    <i className="fa-solid fa-dice-d20 dice20" style={{ opacity: showNav ? 1 : 0, transform: showNav ? 'translateX(0)' : 'translateX(-160px)' }}></i>
-    <i className="fa-solid fa-dice-d6 dice6" style={{ opacity: showNav ? 1 : 0, transform: showNav ? 'translateX(0)' : 'translateX(-160px)' }}></i>
+    <i className="fa-solid fa-dice-d20 dice20" style={{ opacity: showNav ? 1 : 0, transform: showNav ? 'translateX(0)' : 'translateX(-60px)' }}></i>
+    <i className="fa-solid fa-dice-d6 dice6" style={{ opacity: showNav ? 1 : 0, transform: showNav ? 'translateX(0)' : 'translateX(-60px)' }}></i>
       <i onClick={toggleDice20} className="fa-solid fa-hands-asl-interpreting"></i>
-     <i className="fa-solid fa-users playersAmount" style={{ opacity: showNav ? 1 : 0, transform: showNav ? 'translateX(0)' : 'translateX(160px)' }}></i> 
+     <i className="fa-solid fa-users playersAmount" style={{ opacity: showNav ? 1 : 0, transform: showNav ? 'translateX(0)' : 'translateX(60px)' }}></i> 
     </div>
   );
 }
